8be50df54e412c88de4796fbecc8590bc337b832
Luca Rubin <luca.rubin@intesys.it>
2022-10-17 18:38:23 +0200

    Bugfix runtime bundle project start

--- runtime-bundle/src/main/java/it/intesys/activiticloudprocesssimulatorrb/ActivitiCloudProcessSimulatorRbApplication.java
+++ runtime-bundle/src/main/java/it/intesys/activiticloudprocesssimulatorrb/ActivitiCloudProcessSimulatorRbApplication.java
@@ -1,9 +1,11 @@
 package it.intesys.activiticloudprocesssimulatorrb;
 
+import org.activiti.cloud.starter.rb.configuration.ActivitiRuntimeBundle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ActivitiRuntimeBundle
 public class ActivitiCloudProcessSimulatorRbApplication {
 
 	public static void main(String[] args) {

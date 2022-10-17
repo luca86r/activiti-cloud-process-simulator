3fa69c53a820e117b5cefd9916b3cf8594b24598
Luca Rubin <luca.rubin@intesys.it>
2022-10-17 17:53:41 +0200

    Runtime bundle project

---
+++ runtime-bundle/src/main/java/it/intesys/activiticloudprocesssimulatorrb/ActivitiCloudProcessSimulatorRbApplication.java
@@ -0,0 +1,13 @@
+package it.intesys.activiticloudprocesssimulatorrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiCloudProcessSimulatorRbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiCloudProcessSimulatorRbApplication.class, args);
+	}
+
+}
---
+++ runtime-bundle/src/test/java/it/intesys/activiticloudprocesssimulatorrb/ActivitiCloudProcessSimulatorRbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.intesys.activiticloudprocesssimulatorrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiCloudProcessSimulatorRbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
